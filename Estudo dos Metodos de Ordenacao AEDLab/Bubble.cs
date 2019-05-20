af71ec7a19daffc0cc97e3b785f66ede34a525a1
Gustavo Sena <gustavosena98@LIVE.COM>
2019-05-20 12:04:01 -0300

    Adicionar arquivos de projeto.

---
+++ Estudo dos Metodos de Ordenacao AEDLab/Bubble.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_dos_Metodos_de_Ordenacao_AEDLab
+{
+    class Bubble
+    {
+        public int Comparacoes { get; set; }
+        public int Trocas { get; set; }
+
+        public static void Sort(int[] arr)
+        {
+            int repos = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - (i + 1); j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        repos = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = repos;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Estudo dos Metodos de Ordenacao AEDLab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_dos_Metodos_de_Ordenacao_AEDLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
