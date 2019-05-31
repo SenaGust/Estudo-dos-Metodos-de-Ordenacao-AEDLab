4bcc8e10df5c524dc6a4af49f03a0072235561dd
Gustavo Sena <gustavosena98@LIVE.COM>
2019-05-31 07:38:16 -0300

    Adicionado métodos que criam e exibe o vetor

--- Estudo dos Metodos de Ordenacao AEDLab/Program.cs
+++ Estudo dos Metodos de Ordenacao AEDLab/Program.cs
@@ -12,5 +12,29 @@ namespace Estudo_dos_Metodos_de_Ordenacao_AEDLab
         {
 
         }
+
+        #region Vetor
+        static int[] GerarVetor(int tamanho)
+        {
+            int[] vetorGerado = new int[tamanho];
+            Random random = new Random();
+
+            for (int pos = 0; pos < tamanho; pos++)
+            {
+                vetorGerado[pos] = random.Next(tamanho);
+
+            }
+
+            return vetorGerado;
+        }
+
+        static void ExibirVetor(int[] vetor)
+        {
+            for (int pos = 0; pos < vetor.Length; pos++)
+                Console.Write(vetor[pos] + " ");
+
+            Console.WriteLine();
+        }
+        #endregion
     }
 }
