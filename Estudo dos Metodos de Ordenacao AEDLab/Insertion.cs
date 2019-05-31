4d836170e0920cb068aca8f6dc4a85fd42782e33
Gustavo Sena <gustavosena98@LIVE.COM>
2019-05-31 07:36:52 -0300

    Adicionado método InsertionSort

--- Estudo dos Metodos de Ordenacao AEDLab/Insertion.cs
+++ Estudo dos Metodos de Ordenacao AEDLab/Insertion.cs
@@ -11,6 +11,25 @@ namespace Estudo_dos_Metodos_de_Ordenacao_AEDLab
         public int Comparacoes { get; set; }
         public int Trocas { get; set; }
 
-
+        public static void Sort(int[] arr)
+        {
+            int n = arr.Length, i, j, val, flag;
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                        flag = 1;
+                }
+            }
+        }
     }
 }
