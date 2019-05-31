43d5a67c391ed3cbfff91487905fb4220810746b
Gustavo Sena <gustavosena98@LIVE.COM>
2019-05-31 08:23:12 -0300

    Adicionando método de ordenação quicksort

--- Estudo dos Metodos de Ordenacao AEDLab/Quick.cs
+++ Estudo dos Metodos de Ordenacao AEDLab/Quick.cs
@@ -6,11 +6,46 @@ using System.Threading.Tasks;
 
 namespace Estudo_dos_Metodos_de_Ordenacao_AEDLab
 {
-    class Quick
+    class Quick : Ordenacao
     {
-        public int Comparacoes { get; set; }
-        public int Trocas { get; set; }
+        public Quick():base()
+        {
 
+        }
 
-    }
+        public void Sort(int[] arr)
+        {
+            Quicksort(arr, 0, arr.Length-1);
+        }
+        private void Quicksort(int[] v, int inicio, int fim)
+        {
+            int part;
+            if (inicio < fim)
+            {
+                part = Particao(v, inicio, fim);
+                Quicksort(v, inicio, part - 1);
+                Quicksort(v, part + 1, fim);
+            }
+        }        private int Particao(int[] vetor, int inicio, int fim)
+        {
+            int pivot = vetor[fim];
+            int part = inicio - 1;
+
+            for (int pos = inicio; pos < fim; pos++)
+            {
+                base.Comparacoes++;
+                if (vetor[pos] < pivot)
+                {
+                    part++;
+                    //trocar v[part] com v[i]
+                    base.Trocar(vetor, part, pos);
+                    base.Trocas++;
+                }
+            }
+
+            //trocar v[part+1] e v[fim];
+            base.Trocas++;
+            part++;            base.Trocar(vetor, part, fim);
+            return part;
+        }    }
 }
